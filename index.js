7df5f9fc38d1d8d818e6fe4fd27cd5c514931a38
Diana <dianavw8@gmail.com>
2022-12-18 15:35:49 -0500

    Added inquirer engineer/intern questions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { choices } = require('yargs');
 const Manager = require('./lib/Manager');
+const team = []; // Array to store team members
+
 
 inquirer.prompt([
     {
@@ -23,36 +25,99 @@ inquirer.prompt([
       type: 'input',
       name: 'teamManagersOfficeNumber',
       message: 'Enter the Team Manager\'s office number:'
-    },
-    {
-      type: 'list',
-      message: 'Would you like to:',
-      name: 'teamManagersOption',
-      choices: ['add an engineer', 'add an intern', 'finish building my team'],
     }
-
+    
   ]).then(answers => {
     // Use the answers object to create an manager object
     let manager = new Manager(answers.teamManagersName, answers.teamManagersId, answers.teamManagersEmail, answers.teamManagersOfficeNumber);
     console.log(manager);
-    if (answers.teamManagersOption == 'add an engineer') {
-        askEngineerQuestions();
-    }
-    if (answers.teamManagersOption == 'add an intern') {
-        askInternQuestions();
-    }
-    if (answers.teamManagersOption == 'finish building my team') {
-        //dance
-        generateHTML();
-    }
-    
-  });
-  
+   // Add the manager to the team array
+  team.push(manager); 
+  }
 
-
-// function createWebPage(template, content) {
-//     const html = template.replace('{{name}}', content.name)
-//                         .replace('{{id}}', content.name)
-//                         .replace('{{email}}', content.email)
-
-// }
+ // Function to prompt to add an engineer or intern to the team.
+function addTeamMember() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: 'Would you like to add an engineer, an intern or finish building my team?',
+      choices: ['Engineer', 'Intern', 'Finish building my team']
+    }   
+  ]).then(answers => {
+    if (answers.role === 'Engineer') {
+      // Prompt for engineer information
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'engName',
+          message: 'Enter the engineer\'s name:'
+        },
+        {
+          type: 'input',
+          name: 'engId',
+          message: 'Enter the engineer\'s employee ID:'
+        },
+        {
+          type: 'input',
+          name: 'engEmail',
+          message: 'Enter the engineer\'s email address:'
+        },
+        {
+          type: 'input',
+          name: 'engGithub',
+          message: 'Enter the engineer\'s GitHub username:'
+        }
+      ]).then(answers => {
+        // Add the engineer to the team array
+        team.push({
+          name: answers.engName,
+          id: answers.engId,
+          email: answers.engEmail,
+          github: answers.engGithub,
+          role: 'Engineer'
+        });
+        // Prompt to add another team member
+        addTeamMember();
+      });
+    } else if (answers.role === 'Intern') {
+      // Prompt for intern information
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'intName',
+          message: 'Enter the intern\'s name:'
+        },
+        {
+          type: 'input',
+          name: 'intId',
+          message: 'Enter the intern\'s employee ID:'
+        },
+        {
+          type: 'input',
+          name: 'intEmail',
+          message: 'Enter the intern\'s email address:'
+        },
+        {
+          type: 'input',
+          name: 'intSchool',
+          message: 'Enter the intern\'s school:'
+        }
+      ]).then(answers => {
+        // Add the intern to the team array
+        team.push({
+          name: answers.intName,
+          id: answers.intId,
+          email: answers.intEmail,
+          github: answers.intSchool,
+          role: 'Intern'
+        });
+    }  
+        // Prompt to add another team member
+        addTeamMember();
+      });
+    } else if (answers.role === 'Finish building my team') {
+        //Function to return to main menu and exit application
+        process.exit();
+    }    
+  });    
